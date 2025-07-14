5136b49745fec10996797cd92718fdbfedcb93d8
TAMILARASAN M <tamilarasan71845@gmail.com>
2025-07-14 22:42:25 +0530

    created the first project file to learn the Spring Security

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.SpringSecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping()
+    public String home(HttpServletRequest request){
+        return "Home Page "+request.getSession().getId();
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello User";
+    }
+
+    @GetMapping("/about")
+    public  String aboutUs(){
+        return "Tamilarasan!";
+    }
+}
